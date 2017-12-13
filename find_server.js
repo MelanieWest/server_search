a16e61cd1e5f43103c17096b5b22374417c782f3
Melanie West <dhmofan@gmail.com>
2017-12-13 09:37:21 -0500

    start with a test url

--- find_server.js
+++ find_server.js
@@ -4,7 +4,9 @@ exports.findServer=function(serverArray){
     // (lowest priority number) that is available.
     // return a promise that either responds with the server information
     // or rejects with an error message
-
+    
+    var url = serverArray[0].url;
+    
     return new Promise((resolve, reject) => {
         // search for servers, the result of which will either:
         //
